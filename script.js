9774e01b79310d549c4a3ad791224f0cfc6cc9b4
janardhan <seethalamjanardhan958@gmail.com>
2023-01-20 11:40:13 +0530

    weather commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+const newLocal = {
+    apikey: "e57878ee7a46d4ad2f7a78666616bdd3",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apikey
+        )
+        .then((response) => {
+            if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return response.json();
+          })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon,description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/hr";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },  
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+let weather = newLocal;
+
+document
+.querySelector(".search button")
+.addEventListener("click", function(){
+      weather.search()
+})
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if (event.key=="Enter"){
+          weather.search()
+    }
+    
+})
+
+weather.fetchWeather("Denver")
